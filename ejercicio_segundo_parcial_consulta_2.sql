0873fbb9f26ad8b386a747c93b3de325121b73f4
Javier_Sinuka <javier_sinuka@mi.unc.edu.ar>
2025-12-02 13:01:14 -0300

    Adding ejercicio_segundo_parcial_consulta_2

---
+++ ejercicio_segundo_parcial_consulta_2.sql
@@ -0,0 +1,12 @@
+-- Ejercicio examen final
+-- De cada actor en el listado deberá indicar:
+-- - NOMBRE Y APELLIDO
+-- - CANTIDAD DE PELICULAS QUE ACTUO
+-- - CANTIDAD TOTAL DE ALQUILERES QUE SE REALIZARON DE SUS PELICULAS
+-- - CANTIDAD DE ALQUILERES NO DEVUELTOS DE ESE ACTOR
+
+-- HACER UNA SOLA CONSULTA, NO USAR VARIABLES NI TABLAS TEMPORALES
+-- El estado de los datos de ejemplo es uno de los posibles estados,
+-- la consulta debe funcionar correctamente cualquiera sean los datos.
+
+-- --------------------------------------------------------------------------------------------------------------------
